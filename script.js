8e04e83e234895b345ed3327715613d7cd96e6c9
Akemi ohashi <o.a.tabi828@gmail.com>
2022-10-09 20:13:52 +0900

    プラグイン追加編集、写真、menuの配置等変更。

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // 明美エリア
-// $('.fade').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     fade: true,
-//     cssEase: 'linear'
-//   });
+$('.slick-track').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    arrows:false,
+    pauseOnHover:false
+  });
+
+  $('header').fadeIn(2000)
+
+
 
 
 
