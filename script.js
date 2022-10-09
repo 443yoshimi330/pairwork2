eff565c175c60c8bfbe950e3a4eff1818a548127
443YOSHI\yosin <yosin330@yahoo.co.jp>
2022-10-09 20:42:19 +0900

    とりあえずここまでできた分です。

--- script.js
+++ script.js
@@ -54,3 +54,23 @@ $('.slick-track').slick({
 
 
 // 知花さんエリア
+// 映像フェードイン
+$(function () {
+  var $setElm = $('.item'),
+      fadeSpeed = 1500,
+      switchDelay = 5000;
+
+  $setElm.each(function () {
+      var targetObj = $(this);
+      var findUl = targetObj.find('ul');
+      var findLi = targetObj.find('li');
+      var findLiFirst = targetObj.find('li:first');
+
+      findLi.css({ display: 'block', opacity: '0', zIndex: '99' });
+      findLiFirst.css({ zIndex: '100' }).stop().animate({ opacity: '1' }, fadeSpeed);
+
+      setInterval(function () {
+          findUl.find('li:first-child').animate({ opacity: '0' }, fadeSpeed).next('li').css({ zIndex: '100' }).animate({ opacity: '1' }, fadeSpeed).end().appendTo(findUl).css({ zIndex: '99' });
+      }, switchDelay);
+  });
+});
